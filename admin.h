8f66ffbb0fc34a317793247694c84616139d8101
Erfan289 <121449303+Erfan289@users.noreply.github.com>
2023-04-14 12:51:00 -0700

    Update admin.h

--- admin.h
+++ admin.h
@@ -4,11 +4,12 @@
 
 
 #endif // ADMIN_H_INCLUDED
- void Remove_student_record();
- void Add_student_record();
+void Remove_student_record();
+void Add_student_record();
 View_student_record();
 char* Edit_admin_password();
- void Edit_student_grade();
+void Edit_student_grade();
+View_all_records();
 
 
 
