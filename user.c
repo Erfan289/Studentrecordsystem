798aae7085acd6e0949d05edaa63d75659c71312
Erfan289 <121449303+Erfan289@users.noreply.github.com>
2023-04-14 22:01:25 -0700

    Update user.c

--- user.c
+++ user.c
@@ -5,19 +5,33 @@ View_your_record(int Id)
 
     viewYourRecord( Id);
 }
-Edit_your_password(int Id)
+char* Edit_your_password(int Id)
 {
     char new_password[30];
-    printf("please enter your name : \n");
-    scanf("%s",new_name);
+         if(DEcision==0){
+
+Password(0,"null",0);
+}  
+    else if (DEcision==1){
+    printf("please enter your password : \n");
+    scanf("%s",new_password);
     Password(Id, new_password,1)
-    printf("name changed successfully!\n");
+    printf("your password has been changed successfully!\n");}
+    else{
+
+    return Password(Id,"null",2);
+}
 
 }
 Edit_your_name(int Id)
 {
     char new_name[30];
-    printf("please enter your new password \n");
+    if(DEcision==0){
+Name(Id, "null",0);
+  } else{
+    printf("please enter your new name \n");
     scanf("%s",new_name);
-     Name(Id, new_name,1);
+    Name(Id, new_name,1);
+    printf("your name has been changed successfully!\n");
+}
 }
