b8d77d2bd4ac8afca7e48a0cff349083788c8274
Erfan289 <121449303+Erfan289@users.noreply.github.com>
2023-04-14 22:44:12 -0700

    Update program.c

--- program.c
+++ program.c
@@ -4,6 +4,7 @@
 #include"program.h"
 #include "struct.h"
 
+//DONE----------------
 /*this function takes two inputs 1 for editing the password and any thing else to
 set the initial value
 */
@@ -16,21 +17,21 @@ return adminPass;
 else if(decision==1){
     adminPass = (char *) realloc(adminPass, strlen(newPass) + 1);
     strcpy(adminPass, newPass);
-    printf("%s",adminPass);
+
 }else if (decision==2){return adminPass;}
 }
 //this function takes id as an input and return the index of the given id
 myIndex(int id) {
     int index;
     int i;
-    for ( i = 0; i < 20; i++) {
+    for ( i = 0; i < 30; i++) {
 
         if (arr[i].id == id) {
             index = i;
             break;
            }
 
-}if(i==20){
+}if(index!=i){
 printf("wrong id\n");
 return -1;
 } else
@@ -41,14 +42,18 @@ void viewAllRecords() {
 
 
     for (int i = 0; i < 20; i++) {
-           if (arr[i].age==0){
+            if (arr[i].age==0){
+
+
             printf("%d :{----name : %s,password: %s ,ID : %d, degree: %d,age: %d,gender: %c----}\n\n", i+1, arr[i].name,arr[i].password
                    ,arr[i].id, arr[i].degree, arr[i].age, arr[i].gender);
-           }else {
+
+        } else {
                     printf("%d :{----name : %s,password: %s ,ID : %d, degree: %d,age: %d,gender: %c----}\n\n", i+1, name[i],pass[i]
                    ,arr[i].id, arr[i].degree, arr[i].age, arr[i].gender);
 
         }
+
 
         }
 
@@ -56,44 +61,46 @@ void viewAllRecords() {
 
 void viewYourRecord(int id) {
    int index= myIndex(id);
-    printf("your name is : %s\nyour Password is : %s\nyour Degree is: %d\nyour Id is: %d\nyour Age is: %d\nyour Gender is: %c\n", name[index],pass[index],arr[index].degree,arr[index].id,arr[index].age,arr[index].gender);
 
+    printf("Student'sname is : %s\nStudent'sPassword is : %s\nStudent'sDegree is: %d\nStudent'sId is: %d\nStudent'sAge is: %d\nStudent'sGender is: %c\n", name[index],pass[index],arr[index].degree,arr[index].id,arr[index].age,arr[index].gender);
 
 
 }
-
 void viewStudentRecord(int id) {
     int index= myIndex(id);
-    printf("student's Name is : %s\nstudent's Password is : %s\nstudent's Degree is: %d\nstudent's Id is: %d\nstudent's Age is: %d\nstudent's Gender is: %c\n", name[index],pass[index],arr[index].degree,arr[index].id,arr[index].age,arr[index].gender);
+    printf("Your Name is : %s\nYour Password is : %s\nYour Degree is: %d\nYour Id is: %d\nYour Age is: %d\nYour Gender is: %c\n", arr[index].name,arr[index].password,arr[index].degree,arr[index].id,arr[index].age,arr[index].gender);
 
 }
-int addStudent(char *name, char *password, int degree, int id, int age, int gender) {
-    for (int i = 0; i < 20; i++) {
+void addStudent(char *Name, char *Password, int degree, int id, int age, int gender) {
+    int i;
+    for ( i = 0; i < 20; i++) {
         if (arr[i].id == id) {
             printf("this id already exists\n");
-            return 0;
-        }
-        else {
-        if(newSizeOfClass(1)<20){
-    int newIndex=lastIndex();
-   arr[newIndex].name = (char *) malloc(strlen(name) + 1);
-   strcpy(arr[newIndex].name, name);
-   arr[newIndex].password = (char *) malloc(strlen(password) + 1);
-   strcpy(arr[newIndex].password, password);
+            Add_student_record();
+
+        }}
+        if(i==20) {
+   if(newSizeOfClass(1)<20){
+   int newIndex=lastIndex();
+   name[newIndex] = (char *) malloc(strlen(Name) + 1);
+   strcpy(name[newIndex], Name);
+   pass[newIndex] = (char *) malloc(strlen(Password) + 1);
+   strcpy(pass[newIndex], Password);
    arr[newIndex].degree = degree;
-    arr[newIndex].id = id;
-    arr[newIndex].age = age;
-    if(gender==1){
+   arr[newIndex].id = id;
+   arr[newIndex].age = age;
+   if(gender==1){
    arr[newIndex].gender = 'F';
 
    }else if(gender==2){
    arr[newIndex].gender = 'M';
    } else{printf("invalid choice");}
 
-   } printf("the student has been added successfully!\n");}}
+   } printf("the student has been added successfully!\n");
+    } }
 //this function takes two inputs 1 for incrementing ,anything else for decrementing
 int newSizeOfClass(int DECISION){
-     int y;
+
      for (int y  = 0; y <= 20; y++){
     if(arr[y].age!=0){
     counter++;
@@ -128,31 +135,39 @@ void Name(int id, char *newname,int Decision) {
     for(int p=0;p<2;p++){
     name[p]= (char*) malloc(strlen(arr[p].name)+1);
     strcpy(name[p],arr[p].name);
+
     }}
 
     else if(Decision==1) {
         name[myIndex(id)] = (char *) realloc(name[myIndex(id)], strlen(newname) + 1);
         strcpy(name[myIndex(id)], newname);
         arr[myIndex(id)].name=newname;
-     
+
 }}
 char* Password(int id, char *newpass,int Decision) {
-   
+
     if (Decision==0){
     for(int p=0;p<2;p++){
-         Pass[p]= (char*) malloc(strlen(arr[p].password)+1);
-        strcpy(Pass[p],arr[p].password);
+         pass[p]= (char*) malloc(strlen(arr[p].password)+1);
+         strcpy(pass[p],arr[p].password);
+
     }} else if (Decision==1){
-        Pass[myIndex(id)] = (char *) realloc(Pass[myIndex(id)], strlen(newpass) + 1);
-        strcpy(Pass[myIndex(id)], newpass);
+        pass[myIndex(id)] = (char *) realloc(pass[myIndex(id)], strlen(newpass) + 1);
+        strcpy(pass[myIndex(id)], newpass);
         arr[myIndex(id)].password=newpass;
+        printf("%s",pass[myIndex(id)]);
+        }
         else {  for(int i=0;i<20;i++){
 
-           if (id==arr[i].id)
-           return pass[i];
- }
+                        if (id==arr[i].id)
+                        return pass[i];
+
+        }
+        }
 }
-}
+
+
+
 void removeStudent(int id) {
 
 
