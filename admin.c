7da095f6cf11124bcd1e8974093c2b9870b3149d
Erfan289 <121449303+Erfan289@users.noreply.github.com>
2023-04-26 05:58:43 -0700

    Update admin.c

--- admin.c
+++ admin.c
@@ -1,23 +1,22 @@
 #include "admin.h"
 #include "program.h"
  void Add_student_record(){
-char name[20];
-    char student_password[20];
+    char name[30];
+    char student_password[30];
     int student_id,age,grade,gender;
-    printf("Enter Student's Name:");
-    scanf("%s",&name);
-    printf("Enter the Student's ID:");
+    printf("Enter Student's Name:\n");
+    input(name,30);
+    printf("Enter the Student's ID:\n");
     scanf("%d",&student_id);
-    printf("Enter Student's Password:");
-    scanf("%s",&student_password);
-    printf("What is the Student's Gender? -1 for Female/2 for Male-");
+    printf("Enter Student's Password:\n");
+    input(student_password,30);
+    printf("What is the Student's Gender? -1 for Female/2 for Male-\n");
     scanf("%d",&gender);
-    printf("Enter the Student's Age:");
+    printf("Enter the Student's Age:\n");
     scanf("%d",&age);
-    printf("Enter the Student's Grade:");
+    printf("Enter the Student's Grade:\n");
     scanf("%d",&grade);
     addStudent(name,student_password,grade,student_id,age,gender);
-
 
 }
 
@@ -40,9 +39,9 @@ View_student_record()
 char* Edit_admin_password(int decision)
 {   if(decision==1){
     char admin_password[20];
-    char new_admin_password[20];
+    char new_admin_password[30];
     printf("Enter your new password:");
-    scanf("%s",&new_admin_password);
+    input(new_admin_password,30);
     AdminPassword(new_admin_password,1);
     DECIsion=2;
     }
@@ -62,5 +61,3 @@ char* Edit_admin_password(int decision)
 View_all_records(){
 viewAllRecords();
 }
-
-
